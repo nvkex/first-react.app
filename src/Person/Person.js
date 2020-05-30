5ca7dbe43c88267a2681c0e4351eb882deaa4326
Sumit Bhardwaj <40798467+nvkex@users.noreply.github.com>
2020-05-30 12:36:05 +0530

    two-way binding with state

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p>I'm {props.name} and I am {Math.floor(Math.random() * 100)} years old</p>
+    return(
+        <div>
+            <p onClick = {props.click}>I'm {props.name} and I am {Math.floor(Math.random() * 100)} years old</p>
+            <input type="text" onChange={props.changeName} />
+        </div>
+        
+    ) 
 }
 
 export default person;
