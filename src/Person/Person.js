ff79674c85602a890802e1fdd0b75fb595c1a30a
Sumit Bhardwaj <40798467+nvkex@users.noreply.github.com>
2020-05-30 23:10:48 +0530

    manipulating lists

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
     return(
-        <div>
+        <div className="Person">
+            <span onClick={props.delete}>&#10006;</span>
             <p onClick = {props.click}>I'm {props.name} and I am {Math.floor(Math.random() * 100)} years old</p>
-            <input type="text" onChange={props.changeName} />
+            <input type="text" onChange={props.changeName}/>
         </div>
         
     ) 
